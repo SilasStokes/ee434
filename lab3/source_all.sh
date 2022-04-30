298d6201bde7457787ff6ed3a8a917927f0af46d
Silas Stokes <sstokes@sig1.eecs.wsu.edu>
2022-04-29 18:42:38 -0700

    push to start fresh

--- lab3/source_all.sh
+++ lab3/source_all.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
 
- source ictools_generic.sh 
- source cadence_ic618.sh 
- source license-add.sh 
- source synopsys.sh 
+source ictools_generic.sh
+source cadence_ic618.sh
+source license-add.sh
+source mentor_calibre20.sh
+source source_all.sh
+source synopsys.sh
